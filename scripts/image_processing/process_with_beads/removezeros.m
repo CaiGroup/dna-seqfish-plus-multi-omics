0e788cc50be293523a80453d64d47431aa65e38a
Yujing-Bio <56061834+Yujing-Bio@users.noreply.github.com>
2023-03-02 23:16:19 -0800

    Delete scripts directory

--- scripts/image_processing/process_with_beads/removezeros.m
+++
@@ -1,8 +0,0 @@
-function cellin = removezeros(cellin)
-
-log = cellin == 0;
-cellin(log) = [];
-if isempty(cellin) ==1
-    cellin = 0;
-end
-
--- scripts/image_processing/process_with_beads/subtractmulticut.m
+++
@@ -1,9 +0,0 @@
-function I = subtractmulticut(multicutMask, simpleMask)
-% function subractmulticut subtracts the multicut mask from a simple
-% segmentation mask.
-
-    revisedSimpleMask = (double(simpleMask) - 2) .* -1;
-    I = multicutMask .* uint32(revisedSimpleMask);
-
-
-end
